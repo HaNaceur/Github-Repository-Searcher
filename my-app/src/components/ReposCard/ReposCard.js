869de8f9c605beb3a1b5ad8a63a79c2e3b4a2390
Hamza Naceur <hamzanaceur@MacBook-Air-de-Hamza.local>
2022-12-16 11:13:44 +0100

    feat: add url direction to ReposCard

--- my-app/src/components/ReposCard/ReposCard.js
+++ my-app/src/components/ReposCard/ReposCard.js
@@ -1,3 +1,4 @@
+/* eslint-disable camelcase */
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Card, Image } from 'semantic-ui-react';
@@ -8,9 +9,15 @@ function ReposCard({
   name,
   description,
   owner,
+  html_url,
 }) {
   return (
-    <Card className="repos">
+    <Card
+      className="repos"
+      href={html_url}
+      target="_blank"
+      rel="noreferrer"
+    >
       <Image src={owner.avatar_url} wrapped ui={false} />
       <Card.Content>
         <Card.Header>{name}</Card.Header>
@@ -27,6 +34,7 @@ function ReposCard({
 ReposCard.propTypes = {
   name: PropTypes.string.isRequired,
   description: PropTypes.string,
+  html_url: PropTypes.string.isRequired,
   owner: PropTypes.shape({
     login: PropTypes.string.isRequired,
     avatar_url: PropTypes.string.isRequired,
